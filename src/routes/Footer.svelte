9c022fe6dc2850591762d238e8a89e7a192a3239
anotheropensourcefan <94914448+anotheropensourcefan@users.noreply.github.com>
2023-03-29 05:04:01 +0200

    Update Footer.svelte

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,5 +1,3 @@
 <p>
   Control time rather than being controlled by it !<br>
-	My name is Eole, from Earth.<br>
-	I designed this lovely app as part of my <a href="https://www.freecodecamp.org">FreeCodeCamp</a> learning path. This is my first attempt writing Javascript with a framework. So far, Svelte has been amazing.<br>
 </p>
