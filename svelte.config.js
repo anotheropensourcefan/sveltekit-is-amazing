6ce94fdbdbe9b70a388d271b8d2ccd0126390caa
anotheropensourcefan <94914448+anotheropensourcefan@users.noreply.github.com>
2023-03-30 04:10:57 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		paths: {
+      base: process.env.NODE_ENV === "production" ? "/sveltekit-is-amazing" : "",
+    }
 	}
 };
 
